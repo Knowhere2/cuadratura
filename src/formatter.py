69a8c6bf5e03b10c64b486a730c4f7253ef55ccc
Enrique Andrés Martínez Agudelo <enrique.martinezagudelo@externos-co.cencosud.com>
2023-09-05 14:07:42 -0500

    feat: reorganización de dataframes
    Se agregan nuevas columnas a la estructura del DataFrame; además, se agrega funcionalidad que ayuda a la organización de columnas del mismo.

--- src/formatter.py
+++ src/formatter.py
@@ -11,16 +11,29 @@ class Formatter:
             with open(f"{directory}{file}") as _file:
                 json_info = json.loads(_file.read())
                 json_info = json_info['PosLog']['Transaction']['RetailTransaction']['LineItem']
-                json_info = list(filter(lambda x: True if 'POSIdentity' in x else False, json_info))
-                data_frames.append(
-                    pd.DataFrame([{
-                        'PLU': item['POSIdentity']['POSItemID'],
-                        'Base': item['Tax'][0]['BaseAmount'],
-                        item['Tax'][0]['Percent'][:-2]: item['Tax'][0]['Amount']
-                    } for item in json_info])
-                )
+                info_producto = list(filter(lambda x: True if 'POSIdentity' in x else False, json_info))
+                info_tender = list(filter(lambda x: True if 'Tender' in x else False, json_info))
+                info_producto_df = pd.DataFrame([{
+                    'PLU': item['POSIdentity']['POSItemID'],
+                    'Base': float(item['Tax'][0]['BaseAmount']),
+                    item['Tax'][0]['Percent'][:-2]: float(item['Tax'][0]['Amount'])
+                } for item in info_producto])
+                info_tender_df = pd.DataFrame([
+                    {
+                        item['Tender']['TenderType']: float(item['Tender']['Amount']) - float(item['Tender']['TenderChange']['Amount'])
+                    } if 'TenderChange' in item['Tender'] else {
+                        item['Tender']['TenderType']: float(item['Tender']['Amount'])
+                    } for item in info_tender
+                ])
+                info_full_df = pd.concat([info_producto_df, info_tender_df], axis=1)
+                # pd.DataFrame([])
+                data_frames.append(info_full_df)
 
         data_frame = pd.concat(data_frames)
+        headers = data_frame.columns.to_list()
+        headers = cls.make_reindex(headers)
+        data_frame = data_frame.reindex(columns=headers)
+        print(data_frame)
         data_frame.to_excel(f'{filename}.xlsx', index=False)
 
     @classmethod
@@ -41,3 +54,15 @@ class Formatter:
     @property
     def files(self) -> list:
         return self.__files
+
+    @classmethod
+    def make_reindex(cls, headers: list) -> list:
+        headers.sort()
+        plu = headers.pop(headers.index('PLU'))
+        base = headers.pop(headers.index('Base'))
+        return [plu, base] + headers
+
+
+if __name__ == '__main__':
+    a = Formatter()
+    # print(a.make_reindex(['0', '19', '5', 'Base', 'Cash', 'CreditoEspecial', 'PLU', 'Vales']))
